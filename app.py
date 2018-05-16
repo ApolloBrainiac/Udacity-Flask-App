147bd86c2fda6e81e78d2a58080b3c3f71e4f49f
Andrew M. Perez <AxMPerez@gmail.com>
2018-05-16 09:21:12 -0400

    Include JSONIFY API to app backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -32,6 +32,31 @@ items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese',
 item = {'name':'Cheese Pizza','description':'made with fresh cheese',
 'price':'$5.99','course' :'Entree'}
 """
+
+
+# Routes for JSONify
+
+@app.route('/restaurant/JSON/')
+def restaurantJSON():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(restaurants=[r.serialize for r in restaurants])
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON/')
+def menuJSON(restaurant_id):
+    restaurant = session.query(
+        Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(
+        MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def menuItemJSON(restaurant_id, menu_id):
+    menuItem = session.query(
+        MenuItem).filter_by(id=menu_id).one()
+    return jsonify(menuItem=menuItem.serialize)
+
 
 
 # This page will show all restaurants
